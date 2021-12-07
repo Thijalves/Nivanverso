b0c41e5603e3a9335d8bcb8c8f56bb2d21940cc6
Thiago <thijalves@gmail.com>
2021-12-07 20:04:34 -0300

    Cenario base para pulo

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include "raylib.h"
 
-#define playerHspeed 100.0f
+#define gravidade 400
+
+typedef struct {
+    Rectangle rect;
+    int blocking;
+    Color color;
+} Chao;
 
 typedef struct {
     Texture2D texture;
@@ -14,21 +20,64 @@ typedef struct {
     Vector2 position;
     Sprite idle;
     Sprite run;
-
+    float hSpeed;
+    float vSpeed;
 } Player;
 
+void updatePlayer(Player *player, float deltaTime, int *estado){
 
-int main(void){
+    //atualiza o estado de animacao e o movimento horizonal
+    if(IsKeyDown(KEY_D)){
+            player->position.x += player->hSpeed*deltaTime;
+            *estado = 1;
+            player->frame.width = (float)player->run.texture.width/player->run.maxFrames;
+            player->frame.height = (float)player->run.texture.height;
+    }else if(IsKeyDown(KEY_A)){
+        player->position.x -= player->hSpeed*deltaTime;
+        *estado = 1;
+        player->frame.width = -(float)player->run.texture.width/player->run.maxFrames;
+        player->frame.height = (float)player->run.texture.height;
+    }else{
+        *estado = 0;
+        player->frame.width = (float)player->idle.texture.width/player->idle.maxFrames;
+        player->frame.height = (float)player->idle.texture.height;
+    }
+
+    int hitObstacle = 0;
+
+    /* player->position.y += player->vSpeed*deltaTime;
+    player->vSpeed += gravidade*deltaTime;  */
+
+
+}
+
+
+int main(void){   //ao mudar de animacao nos mudamos a largura e altura do frame e a textura
    
     const int screenWidth = 800;
     const int screenHeight = 450;
 
     InitWindow(screenWidth, screenHeight, "Nivan no nivanverso");
 
-    //ao mudar de animacao nos mudamos a largura e altura do frame e a textura
+    //inicializa o chao
+    Chao chao = {0};
+    chao.rect.x = 300;
+    chao.rect.y = 260;
+    chao.rect.width = 200;
+    chao.rect.height = 5;
+    chao.color = GREEN;
+    //sombra
+    Chao sombra = {0};
+    sombra.rect.x = 302;
+    sombra.rect.y = 262;
+    sombra.rect.width = 200;
+    sombra.rect.height = 5;
+    sombra.color = DARKGRAY;
 
     //instancia o player com a animacao idle
     Player player = {0};
+    player.hSpeed = 125;
+    player.vSpeed = 0;
     player.idle.texture = LoadTexture("../resources/idle.png");
     player.idle.maxFrames = 12;
     player.position.x = screenWidth/2;
@@ -60,36 +109,28 @@ int main(void){
 
         int playerState = 0; // 0=idle 1=run 2 = runL
 
-        if(IsKeyDown(KEY_D)){
-            player.position.x += playerHspeed*deltaTime;
-            playerState = 1;
-            player.frame.width = (float)player.run.texture.width/player.run.maxFrames;
-            player.frame.height = (float)player.run.texture.height;
-        }else if(IsKeyDown(KEY_A)){
-            player.position.x -= playerHspeed*deltaTime;
-            playerState = 1;
-            player.frame.width = -(float)player.run.texture.width/player.run.maxFrames;
-            player.frame.height = (float)player.run.texture.height;
-        }else{
-            playerState = 0;
-            player.frame.width = (float)player.idle.texture.width/player.idle.maxFrames;
-            player.frame.height = (float)player.idle.texture.height;
+        updatePlayer( &player, deltaTime, &playerState);
+
+        if(IsKeyPressed(KEY_R)){
+            player.position.y = 0;
+            player.vSpeed = 0;
         }
       
         BeginDrawing();
             BeginMode2D(camera);
 
-            ClearBackground(RAYWHITE);
+            ClearBackground(BLUE);
+            
+            DrawRectangleRec(sombra.rect, sombra.color);
+            DrawRectangleRec(chao.rect, chao.color);
 
             timer += GetFrameTime();
-
-            if(timer >= 0.1){
+            if(timer >= 0.075){
                 timer=0;
                 frame++;
             }
 
             frame = frame % player.idle.maxFrames;
-    
             player.frame.x = (player.frame.width *frame);
 
             if(playerState){
