a338cc1759eda85d59a152cd4110392de070f5b0
Thiago <thijalves@gmail.com>
2021-12-11 17:22:41 -0300

    confirmar

--- main.c
+++ main.c
@@ -31,6 +31,12 @@ typedef struct {
     char facingDirection; //direita=1 esquerda=0
 } Player;
 
+typedef enum {
+    MENU = 0,
+    PLAY,
+    CREDITS,
+} Selection;
+
 //Funcao pra atualizar as variaveis do player
 void updatePlayer(Player *player, float deltaTime, EnvItem *envItems, int envItemsLength){
 
@@ -189,8 +195,12 @@ void updateCamera(Camera2D *camera, Player *player, int screenWidth, int screenH
 
 int main(void){   //ao mudar de animacao nos mudamos a largura e altura do frame e a textura
 
-    FILE *mapFile = fopen("../fase.txt","r");
+    //variaveis do menu
+    int framesCounter=0;
+    Selection Option = MENU;
 
+    //carerga os arquivos de mapa
+    FILE *mapFile = fopen("../fase.txt","r");
     if(mapFile == NULL)
         printf("erro ao abrir arquivo\n");
    
@@ -209,7 +219,6 @@ int main(void){   //ao mudar de animacao nos mudamos a largura e altura do frame
     fscanf(mapFile, "width: %d", &mapWidth);
 
     printf("Largura do mapa: %d\n", mapWidth);
-
 
     EnvItem envItems[1000];
 
@@ -289,6 +298,20 @@ int main(void){   //ao mudar de animacao nos mudamos a largura e altura do frame
     player.falling.texture = LoadTexture("../resources/falling.png");
     player.falling.maxFrames = 1;
 
+    //carrega a fonte
+    Font font = LoadFontEx("../assets/font1.ttf", 50, 0, 0);
+    SetTextureFilter(font.texture, TEXTURE_FILTER_TRILINEAR);
+
+    //carrega o .txt
+    char *text; text = NULL;
+    text = LoadFileText("../data/about.txt");
+
+    char *text2; text2 = NULL;
+    text2 = LoadFileText("../data/credits.txt");
+
+    char *text3; text3 = NULL;
+    text3 = LoadFileText("../data/help.txt");
+
     float timer = 0;
     int frame = 0;
 
@@ -300,53 +323,120 @@ int main(void){   //ao mudar de animacao nos mudamos a largura e altura do frame
 
     SetTargetFPS(60);
 
-    // Main game loop
-    while (!WindowShouldClose())   {
-        float deltaTime = GetFrameTime();
+    char fechar = 0;
 
-        updateCamera(&camera, &player, screenWidth, screenHeight);
-
-        updatePlayer(&player, deltaTime, envItems, envItemsLength);
-
-        // printf("%f\n", player.position.x);
-
-        if(IsKeyPressed(KEY_R)){
-            player.position.y = 100;
-            player.position.x = 0;
-            player.vSpeed = 0;
-        }
-      
-        BeginDrawing();
-            BeginMode2D(camera);
-
-                ClearBackground(BLUE);
-
-                //for (int i = 0; i < envItemsLength; i++) DrawRectangleRec(envItems[i].rect, envItems[i].color); //desenhna os obstaculos
-                for (int i = 0; i < envItemsLength; i++) DrawRectangleRec(envItems[i].rect, envItems[i].color); //desenhna os obstaculos
-
-                //conta os frames para animacao
-                timer += GetFrameTime();
-                if(timer >= 0.075){
-                    timer=0;
-                    frame++;
+    //detecta o que foi pressionado
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        switch(Option){
+            case MENU:
+                if(IsKeyPressed(KEY_ENTER)){
+                    Option = PLAY;
+                } else if(IsKeyPressed(KEY_C)){
+                    Option = CREDITS;
                 }
-                frame = frame % player.idle.maxFrames;
-                player.frame.x = (player.frame.width *frame);
+                break;
+        }
 
-                drawPlayer(&player); //desenha o player
+        switch(Option){
 
-            EndMode2D();    
+            case PLAY:
+            {
+                while (!WindowShouldClose()){
+
+                    float deltaTime = GetFrameTime();
+
+                    updateCamera(&camera, &player, screenWidth, screenHeight);
+
+                    updatePlayer(&player, deltaTime, envItems, envItemsLength);
+
+                    if(IsKeyPressed(KEY_R)){
+                        player.position.y = 100;
+                        player.position.x = 0;
+                        player.vSpeed = 0;
+                    }
+
+                    BeginDrawing();
+                    BeginMode2D(camera);
+
+                    ClearBackground(BLUE);
+
+                    //for (int i = 0; i < envItemsLength; i++) DrawRectangleRec(envItems[i].rect, envItems[i].color); //desenhna os obstaculos
+                    for (int i = 0; i < envItemsLength; i++) DrawRectangleRec(envItems[i].rect, envItems[i].color); //desenhna os obstaculos
+
+                    //conta os frames para animacao
+                    timer += GetFrameTime();
+                    if(timer >= 0.075)
+                    {
+                        timer=0;
+                        frame++;
+                    }
+                    frame = frame % player.idle.maxFrames;
+                    player.frame.x = (player.frame.width *frame);
+
+                    drawPlayer(&player); //desenha o player
+
+                    EndMode2D();
+                    EndDrawing();
+
+                }
+
+                UnloadTexture(player.idle.texture);
+                UnloadTexture(player.run.texture);
+                UnloadTexture(player.runLeft.texture);
+                UnloadTexture(player.falling.texture);
+                UnloadTexture(player.jumping.texture);
+                UnloadFont(font);
+                UnloadFileText(text);
+                UnloadFileText(text2);
+                UnloadFileText(text3);
+                CloseWindow();
+
+            } break;
+
+            case CREDITS:
+            {
+                if ("../data/credits.txt" != NULL)
+                {
+                    if (text2)
+                    {
+                        while(!WindowShouldClose()){
+                            BeginDrawing();
+                            ClearBackground(WHITE);
+                            DrawText(text2, 190, 200, 20, BLUE);
+                            DrawText("E - fechar\nESC - retornar", 400, 250, 20, BLACK);
+                            EndDrawing();
+                            if(IsKeyPressed(KEY_ESCAPE)){
+                                Option = MENU;
+                                break;
+                            }
+                        }
+                    }
+                }
+            } break;
+
+        }
+
+        framesCounter+=3; //a cada frame, (x) letras sao printadas,  quanto maior, mais rapido
+        BeginDrawing();
+        
+        ClearBackground(SKYBLUE);
+        DrawTextEx(font, "NIVAN no Nivanverso", (Vector2){175, 100}, 35, 8, YELLOW);
+        DrawTextEx(font, TextSubtext("INICIAR - Enter\n CREDITOS - C", 0, framesCounter/5), (Vector2){260, 225}, 35, 8, BLACK);
+        
         EndDrawing();
-
     }
 
-    UnloadTexture(player.idle.texture);
-    UnloadTexture(player.run.texture);
-    UnloadTexture(player.runLeft.texture);
-    UnloadTexture(player.falling.texture);
-    UnloadTexture(player.jumping.texture);
-
-    CloseWindow();                
+        UnloadTexture(player.idle.texture);
+        UnloadTexture(player.run.texture);
+        UnloadTexture(player.runLeft.texture);
+        UnloadTexture(player.falling.texture);
+        UnloadTexture(player.jumping.texture);
+        UnloadFont(font);
+        UnloadFileText(text);
+        UnloadFileText(text2);
+        UnloadFileText(text3);
+        CloseWindow();
 
     return 0;
 }
