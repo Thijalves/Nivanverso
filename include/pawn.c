1f8c812d9a62f568a63c25296b72f1e0a0ddd8c5
cebms <cemendes@gmail.com>
2021-12-17 19:36:40 -0300

    adicionamos logica para mais de um peao

--- include/pawn.c
+++ include/pawn.c
@@ -1,24 +1,32 @@
 #include "pawn.h"
+#include "audio.h"
 
-void initiatePawn(Enemy *pawn, Player *player){
+void initiatePawn(Enemy *pawn, Vector2 position){
     pawn->texture = LoadTexture("./textures/inimigo/peao.png");
-    pawn->rectangle.x = player->position.x + 64;
-    pawn->initialPosition.x = player->position.x + 256;
-    pawn->rectangle.y = player->position.y - 40;
+    // pawn->rectangle.x = player->position.x + 64;
+    // pawn->rectangle.x = player->position.x + 256;
+    pawn->rectangle.x = position.x;
+    // pawn->initialPosition.x = pawn->rectangle.x + 192;
+    pawn->initialPosition.x = pawn->rectangle.x;
+
+    pawn->rectangle.y = position.y;
     pawn->initialPosition.y = pawn->rectangle.y;
+
     pawn->rectangle.width = 20;
     pawn->rectangle.height = 40;
     pawn->direction = 1;
     pawn->speed = 50;
     pawn->color = ORANGE;
 }
-void hitPawn(Enemy *pawn, Player *player){
+void hitPawn(Enemy *pawn, Player *player, Audio *audio){
+    audio->jump = LoadSound("audio/jump.mp3");
     if(player->position.x + player->frame.width >= pawn->rectangle.x &&
         player->position.x <= pawn->rectangle.x + pawn->rectangle.width &&
         player->position.y >= pawn->rectangle.y){
 
         if(player->position.y - pawn->rectangle.y < 5){
             player->vSpeed = -player->jumpS;
+            PlaySound(audio->jump);
         }else{
             player->color = GRAY;
             player->vida--;
--- include/updatePlayer.h
+++ include/updatePlayer.h
@@ -4,10 +4,11 @@
 #include "raylib.h"
 #include "structPlayer.h"
 #include "envItemsStruct.h"
+#include "audio.h"
 
 #define gravity 350
 
 //Funcao pra atualizar as variaveis do player
-void updatePlayer(Player *player, float deltaTime, EnvItem *envItems, int envItemsLength);
+void updatePlayer(Player *player, float deltaTime, EnvItem *envItems, int envItemsLength, Audio *audio);
 
 #endif
